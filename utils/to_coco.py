64d45c9d58f6bb28fa715bc4e56d45dd6f22dfcc
Hamed <147453648+cloner174@users.noreply.github.com>
2025-01-16 02:18:53 +0330

    Update to_coco.py

--- utils/to_coco.py
+++ utils/to_coco.py
@@ -218,23 +218,115 @@ def get_df_coco(label_dir, image_dir, dir_of='train'):
     return df
 
 
+def convert_bbox_format(bboxes, image_size):
+    """
+    Convert [x_center, y_center, width, height] to [xmin, ymin, xmax, ymax]
+    """
+    image_width, image_height = image_size
+    converted = []
+    for bbox in bboxes:
+        x_center, y_center, width, height = bbox
+        xmin = (x_center - width / 2) * image_width
+        ymin = (y_center - height / 2) * image_height
+        xmax = (x_center + width / 2) * image_width
+        ymax = (y_center + height / 2) * image_height
+        converted.append([xmin, ymin, xmax, ymax])
+    return converted
 
-def convert_bbox_format(boxes, image_size):
+
+
+def get_df2(label_dir, image_dir, dir_of='train'):
+    df = []
+    for filename in os.listdir(label_dir):
+        if filename.endswith('.txt'):
+            filepath = os.path.join(label_dir, filename)
+            image_path = os.path.join(image_dir, filename.replace('.txt', '.jpg'))
+            
+            if os.path.isfile(image_path):
+                image_name = filename.replace('.txt', '.jpg')
+            else:
+                image_path = os.path.join(image_dir, filename.replace('.txt', '.png'))
+                if os.path.isfile(image_path):
+                    image_name = filename.replace('.txt', '.png')
+                else:
+                    raise Exception(f'No Image Found for : {filename}')
+            
+            image = Image.open(image_path).convert("RGB")
+            image_width, image_height = image.size
+            
+            with open(filepath, 'r') as file:
+                for line in file:
+                    parts = line.strip().split()
+                    obj_class = parts[0]
+                    x_center = float(parts[1])
+                    y_center = float(parts[2])
+                    width = float(parts[3])
+                    height = float(parts[4])
+                    box = [x_center, y_center, width, height]
+                    
+                    converted_box = convert_bbox_format([box], (image_width, image_height))[0]
+                    
+                    df.append({
+                        'image_name': image_name,
+                        'image_path': image_path,
+                        'xmin': converted_box[0],
+                        'ymin': converted_box[1],
+                        'xmax': converted_box[2],
+                        'ymax': converted_box[3],
+                        'class': map_of_classes[int(obj_class)],
+                        'width': image_width,
+                        'height': image_height
+                    })
     
-    converted_boxes = []
-    img_width, img_height = image_size
-    for box in boxes:
-        x_center, y_center, width, height = box
-        xmin = (x_center - width / 2) * img_width
-        ymin = (y_center - height / 2) * img_height
-        xmax = (x_center + width / 2) * img_width
-        ymax = (y_center + height / 2) * img_height
-        xmin = max(0, xmin)
-        ymin = max(0, ymin)
-        xmax = min(img_width, xmax)
-        ymax = min(img_height, ymax)
-        converted_boxes.append([xmin, ymin, xmax, ymax])
+    df = pd.DataFrame(df)
     
-    return converted_boxes
+    coco = {
+        "images": [],
+        "annotations": [],
+        "categories": []
+    }
+    
+    classes = sorted(df['class'].unique())
+    category_mapping = {cls: idx + 1 for idx, cls in enumerate(classes)}
+    categories = [{"id": idx, "name": cls} for cls, idx in category_mapping.items()]
+    coco["categories"] = categories
+    
+    image_id = 1
+    annotation_id = 1
+    images_dict = {}
+    for _, row in df.iterrows():
+        img_name = row['image_name']
+        if img_name not in images_dict:
+            image_info = {
+                "id": image_id,
+                "file_name": img_name,
+                "width": int(row['width']),
+                "height": int(row['height'])
+            }
+            coco["images"].append(image_info)
+            images_dict[img_name] = image_id
+            image_id += 1
+        
+        bbox_width = row['xmax'] - row['xmin']
+        bbox_height = row['ymax'] - row['ymin']
+        area = bbox_width * bbox_height
+        
+        annotation = {
+            "id": annotation_id,
+            "image_id": images_dict[img_name],
+            "category_id": category_mapping[row['class']],
+            "bbox": [row['xmin'], row['ymin'], bbox_width, bbox_height],  # Correct format
+            "area": area,
+            "iscrowd": 0
+        }
+        coco["annotations"].append(annotation)
+        annotation_id += 1
+    
+    with open(f'coco_{dir_of}.json', 'w') as f:
+        json.dump(coco, f, indent=4)
+    
+    df.to_csv(f'df_{dir_of}.csv', index=False)
+    
+    return df
 
 #cloner174
