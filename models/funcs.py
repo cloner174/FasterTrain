4a3ca27c65f9af1e7193b9372ba09ba434cb8d40
Hamed <147453648+cloner174@users.noreply.github.com>
2025-01-16 03:06:21 +0330

    Update funcs.py

--- models/funcs.py
+++ models/funcs.py
@@ -87,3 +87,52 @@ def evaluate(model, data_loader, device, coco_gt):
     )
     
     return stats, precision, recall, f1
+
+
+
+def evaluate2(model, data_loader, device, coco_gt):
+    """
+       stats: List of 12 COCO stats from coco_eval.stats
+              [AP_50_95, AP_50, AP_75, AP_small, AP_medium, AP_large,
+               AR_1, AR_10, AR_100, AR_small, AR_medium, AR_large]
+    """
+    model.eval()
+    results = []
+    with torch.no_grad():
+        for images, targets in tqdm(data_loader, desc="Evaluation"):
+            images = list(img.to(device) for img in images)
+            outputs = model(images)
+            for i, output in enumerate(outputs):
+                image_id = targets[i]['image_id'].item()
+                for box, score, label in zip(output['boxes'], output['scores'], output['labels']):
+                    box = box.to('cpu').numpy()
+                    score = score.to('cpu').item()
+                    label = label.to('cpu').item()
+                    
+                    x1, y1 = box[0], box[1]
+                    x2, y2 = box[2] - box[0], box[3] - box[1]
+                    result = {
+                        "image_id": image_id,
+                        "category_id": int(label),
+                        "bbox": [ box[0], box[1], box[2] - box[0], box[3] - box[1] ],
+                        "score": score
+                    }
+                    results.append(result)
+    
+    if len(results) == 0:
+
+        empty_stats = [0.0]*12
+        return empty_stats, 0.0, 0.0, 0.0
+    
+    coco_dt = coco_gt.loadRes(results)
+    coco_eval = COCOeval(coco_gt, coco_dt, 'bbox')
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    coco_eval.summarize()
+    stats = coco_eval.stats
+    
+    precision, recall, f1 = compute_pr_f1(
+        results, coco_gt, iou_threshold=0.5, conf_threshold=0.5
+    )
+    
+    return stats, precision, recall, f1
